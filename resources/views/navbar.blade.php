0276794257b90202aa83483d5293ddd3757d5736
=Stevan26 <=stevanpanjaitan433@gmail.com>
2026-05-06 00:02:23 +0700

    membuat link pada nvabar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -8,10 +8,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link "href="#">Home</a>
+                        <a class="nav-link "href="{{route('pasien.index')}}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Daftar pasien</a>
+                        <a class="nav-link" href="{{route('pasien.create')}}">Daftar pasien</a>
                     </li>
                    
                 </ul>
